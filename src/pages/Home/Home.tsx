fca2c7af8a45874be4769f135e8c8221d6a4a0df
Qweffy <qweffooo@gmail.com>
2024-05-05 15:15:06 -0300

    feat: update Home component for filter within categories

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,26 +1,55 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { FlatList } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
-import  CollapsiblePanel  from '@components/CollapsiblePanel'
+import CollapsiblePanel from '@components/CollapsiblePanel'
+import { useSelector, useDispatch } from 'react-redux'
+import { selectAllCategories, categoriesLoading, categoriesReceived, categoriesFailed } from '@features/categoriesSlice'
+import CategorySelector from '@components/CategorySelector'
+import { useProductFilter } from '@hooks/useProductFilter'
+import { IndexPath } from '@ui-kitten/components'
 import Product from '@models/Products'
-import { useSelector } from 'react-redux'
-import { RootState } from '../../store/store'
-import { selectAllProducts } from '@features/productsSlice'
-
+import { useGetCategoriesQuery } from '@store/services/products'
 
 export const Home = () => {
-  const products = useSelector((state: RootState) => selectAllProducts(state))
-  const renderItem = ({ item }: { item: Product }) => (
-    <CollapsiblePanel product={item}/>
-  )
+  const dispatch = useDispatch()
+  const { data: categoriesData, isError: isCategoriesError, error: categoriesError } = useGetCategoriesQuery()
+  const categories: string[] = useSelector(selectAllCategories)
+  const [displayLimit, setDisplayLimit] = useState<number>(5)
+  const [selectedCategory, setSelectedCategory] = useState<string | null>(null)
+  const displayedProducts: Product[] = useProductFilter(selectedCategory, displayLimit)
+
+  useEffect(() => {
+    if (isCategoriesError) {
+      dispatch(categoriesFailed(categoriesError?.toString() || 'Unknown error'))
+    } else if (categoriesData) {
+      dispatch(categoriesReceived(categoriesData))
+    } else {
+      dispatch(categoriesLoading())
+    }
+  }, [categoriesData, isCategoriesError, categoriesError, dispatch])
+
+  const handleLoadMore = () => setDisplayLimit(limit => limit + 5)
+
+  const handleCategoryChange = (index: IndexPath | IndexPath[]) => {
+    if (Array.isArray(index)) {
+      const firstIndex = index[0]
+      setSelectedCategory(firstIndex.row === 0 ? null : categories[firstIndex.row - 1])
+    } else {
+      setSelectedCategory(index.row === 0 ? null : categories[index.row - 1])
+    }
+  }
 
   return (
     <SafeAreaView style={{ flex: 1 }}>
-    <FlatList
-        data={products}
-        renderItem={renderItem}
+      <CategorySelector categories={categories} selectedCategory={selectedCategory} onChange={handleCategoryChange} />
+      <FlatList
+        data={displayedProducts}
+        renderItem={({ item }) => <CollapsiblePanel product={item} />}
         keyExtractor={item => item.id.toString()}
+        onEndReached={handleLoadMore}
+        onEndReachedThreshold={0.5}
       />
     </SafeAreaView>
   )
 }
+

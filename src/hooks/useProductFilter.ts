2baccb8dc1fc221015f96b28bd9b9823e1fcf6d8
Qweffy <qweffooo@gmail.com>
2024-05-05 15:14:52 -0300

    feat: categories filter logic

---
+++ src/hooks/useProductFilter.ts
@@ -0,0 +1,18 @@
+import { useState, useEffect } from 'react'
+import { useSelector } from 'react-redux'
+import { selectAllProducts } from '@features/productsSlice'
+import Product from '@models/Products'
+
+export const useProductFilter = (selectedCategory: string | null, displayLimit: number): Product[] => {
+  const allProducts: Product[] = useSelector(selectAllProducts)
+  const [displayedProducts, setDisplayedProducts] = useState<Product[]>([])
+
+  useEffect(() => {
+    const filteredProducts = selectedCategory
+      ? allProducts.filter(product => product.category === selectedCategory)
+      : allProducts
+    setDisplayedProducts(filteredProducts.slice(0, displayLimit))
+  }, [allProducts, selectedCategory, displayLimit])
+
+  return displayedProducts
+}

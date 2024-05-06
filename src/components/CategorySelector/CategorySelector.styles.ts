2336905efa3b56fea41fb32121f433315483e859
Qweffy <qweffooo@gmail.com>
2024-05-06 07:34:00 -0300

    feat: add Category selector style

---
+++ src/components/CategorySelector/CategorySelector.styles.ts
@@ -0,0 +1,11 @@
+import { StyleSheet, ViewStyle } from 'react-native'
+
+type CategorySelectorStyleType = {
+  container: ViewStyle
+}
+
+export default StyleSheet.create<CategorySelectorStyleType>({
+  container: {
+    margin:10,
+  },
+})

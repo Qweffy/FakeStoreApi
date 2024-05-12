d74fef7303825bb9dc5995558889b96bb12bce04
Qweffy <qweffooo@gmail.com>
2024-05-12 17:07:33 -0300

    feat: create Cart page

---
+++ src/pages/Cart/Cart.types.ts
@@ -0,0 +1,6 @@
+import { NavigationProp } from '@react-navigation/native'
+import { RootStackParamList } from '@models/navigationTypes'
+
+export type CartProps = {
+  navigation: NavigationProp<RootStackParamList>;
+}

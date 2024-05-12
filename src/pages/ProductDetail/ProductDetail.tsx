67518553187ba9b0caab0f82b9c2a62ff14c281a
Qweffy <qweffooo@gmail.com>
2024-05-12 19:22:04 -0300

    feat: Add to Cart button

--- src/pages/ProductDetail/ProductDetail.tsx
+++ src/pages/ProductDetail/ProductDetail.tsx
@@ -1,15 +1,25 @@
 import React from 'react'
-import { Image } from 'react-native'
+import { Image, ScrollView } from 'react-native'
 import styles from './ProductDetail.styles'
-import { Icon, Layout, Text } from '@ui-kitten/components'
+import { Button, Layout, Text } from '@ui-kitten/components'
 import { TopNavigator } from '@navigation/navigators/TopNavigator'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { ProductDetailProps } from '@pages/ProductDetail/ProductDetail.types'
+import { useDispatch } from 'react-redux'
+import { addItem } from '@features/cartSlice'
 
 export const ProductDetail = ({ route, navigation }: ProductDetailProps) => {
   const { product } = route.params
+  const dispatch = useDispatch()
+
+  const handleAddToCart = () => {
+    dispatch(addItem(product))
+  }
+
+
   return (
     <SafeAreaView style={{ flex: 1 }}>
+      <ScrollView>
       <TopNavigator navigation={navigation} />
       <Layout style={styles.expandedLayout}>
         <Image source={{ uri: product.image }} style={styles.expandedImage} resizeMode={'contain'} />
@@ -25,9 +35,10 @@ export const ProductDetail = ({ route, navigation }: ProductDetailProps) => {
             <Text style={styles.label}>Rating:</Text>
             <Text style={styles.value}>{product.rating.rate} ({product.rating.count} reviews)</Text>
           </Layout>
-          <Icon name={'minus-outline'} style={styles.icon} />
         </Layout>
+        <Button onPress={handleAddToCart}>Add to Cart</Button>
       </Layout>
+      </ScrollView>
     </SafeAreaView>
   )
 }

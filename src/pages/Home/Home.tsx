1aa8b1b22811834bff26c847696259f2ab3d6d95
Qweffy <qweffooo@gmail.com>
2024-05-12 17:07:19 -0300

    feat: type Home page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,20 +1,23 @@
 import React from 'react'
 import { FlatList } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
-import CollapsiblePanel from '@components/CollapsiblePanel'
+import ProductCard from '@components/ProductCard'
 import CategorySelector from '@components/CategorySelector'
 import { useHome } from './useHome'
 import styles from './Home.styles'
+import { TopNavigator } from '@navigation/navigators/TopNavigator'
+import { HomeProps } from './Home.types'
 
-export const Home = () => {
+export const Home = ({ navigation }:HomeProps) => {
   const { categories, displayedProducts, handleLoadMore, handleCategoryChange, selectedCategory } = useHome()
 
   return (
     <SafeAreaView style={styles.container}>
+      <TopNavigator navigation={navigation} />
       <CategorySelector categories={categories} selectedCategory={selectedCategory} onChange={handleCategoryChange} />
       <FlatList
         data={displayedProducts}
-        renderItem={({ item }) => <CollapsiblePanel product={item} />}
+        renderItem={({ item }) => <ProductCard product={item} />}
         keyExtractor={item => item.id.toString()}
         onEndReached={handleLoadMore}
         onEndReachedThreshold={0.5}

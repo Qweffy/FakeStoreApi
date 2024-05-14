944f4afd4e79d535599562cacf484b6470d678cc
Qweffy <qweffooo@gmail.com>
2024-05-13 23:26:58 -0300

    feat: add persistor in cart

--- App.tsx
+++ App.tsx
@@ -3,8 +3,9 @@ import * as eva from '@eva-design/eva'
 import { ApplicationProvider, IconRegistry } from '@ui-kitten/components'
 import { EvaIconsPack } from '@ui-kitten/eva-icons'
 import { SafeAreaProvider } from 'react-native-safe-area-context'
-import { store } from '@store/store'
+import { store, persistor } from '@store/store'
 import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
 import { LoadingHandler } from '@pages/LoadingHandler/LoadingHandler'
 import { AppNavigator } from './src/navigation'
 
@@ -14,9 +15,11 @@ const MyApp = () => {
       <IconRegistry icons={EvaIconsPack} />
       <ApplicationProvider {...eva} theme={eva.light}>
         <Provider store={store}>
-          <LoadingHandler>
-            <AppNavigator />
-          </LoadingHandler>
+          <PersistGate loading={null} persistor={persistor}>
+            <LoadingHandler>
+              <AppNavigator />
+            </LoadingHandler>
+          </PersistGate>
         </Provider>
       </ApplicationProvider>
     </SafeAreaProvider>
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,19 +1,39 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore, combineReducers } from '@reduxjs/toolkit'
+import { persistStore, persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from 'redux-persist'
+import AsyncStorage from '@react-native-async-storage/async-storage'
 import { productsApi } from './services/products'
 import productsReducer from '../features/productsSlice'
 import categoriesReducer from '../features/categoriesSlice'
 import cartReducer from '../features/cartSlice'
 
-export const store = configureStore({
-  reducer: {
-    [productsApi.reducerPath]: productsApi.reducer,
-    products: productsReducer,
-    categories: categoriesReducer,
-    cart: cartReducer
-  },
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(productsApi.middleware),
+const rootPersistConfig = {
+  key: 'root',
+  storage: AsyncStorage,
+  whitelist: ['cart'],
+}
+
+const rootReducer = combineReducers({
+  [productsApi.reducerPath]: productsApi.reducer,
+  products: productsReducer,
+  categories: categoriesReducer,
+  cart: cartReducer,
 })
+
+const persistedReducer = persistReducer(rootPersistConfig, rootReducer)
+
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }).concat(productsApi.middleware),
+})
+
+const persistor = persistStore(store)
+
+export { store, persistor }
 
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
